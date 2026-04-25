c0e71a6c1690fba9082f7d82f49131b58232a764
ShoroukShawky <324243558@sha.edu.eg>
2026-04-25 18:38:40 +0300

    Ex01

---
+++ ConsoleHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV03
+{
+    internal static class ConsoleHelper
+    {
+        public static void PrintList<T>(string listName, List<T> items)
+        {
+            Console.WriteLine($"{listName} : [{string.Join(", ", items)}]");
+
+        }
+
+        public static void PrintHashSet<T>(string setName, HashSet<T> items)
+        {
+            Console.WriteLine($"{setName} : [{string.Join(", ", items)}]");
+
+        }
+
+        public static void PrintCollection<T>(string colName, IEnumerable<T> items)
+        {
+            Console.WriteLine($"{colName} : [{string.Join(", ", items)}]");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,38 @@
         static void Main(string[] args)
         {
             #region Ex01
+            // 1
+            List<int> list = new() {85,92,95,88,70,100,65 };
 
+            // 2
+            ConsoleHelper.PrintList("Grades", list);
+            Console.WriteLine($"List Count = {list.Count}");
+            Console.WriteLine($"first grade = {list.First()}");
+            Console.WriteLine($"last grade = {list.Last()}");
+
+            // 3
+            list.Sort();
+            ConsoleHelper.PrintList("Grades", list);
+
+            // 4
+            int firstAbov90 = list.Find(n => n > 90);
+            Console.WriteLine($"first grade above 90 = {firstAbov90}");
+
+            //5
+            List<int> failingGrades = list.Where(n => n < 75).ToList();
+            ConsoleHelper.PrintList("Failing Gardes", failingGrades);
+
+            //6
+            list.RemoveAll(n => n < 75);
+            ConsoleHelper.PrintList("after remove all failing grades =", list);
+
+            // 7
+            bool gradeEquals100 = list.Exists(n => n == 100);
+            Console.WriteLine($"Contains 100 ? {gradeEquals100}");
+
+            //8
+            List<string> gradeStrings = [..list.Select(g => $"Grade: {g}")];
+            ConsoleHelper.PrintList("Sting List", gradeStrings);
             #endregion
         }
     }
